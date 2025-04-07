aa7120b1229ae12dc573acf82407fcc34aca99c9
Derstine <derstinedylan@gmail.com>
2025-04-07 13:04:41 -0400

    new dashboard ui template

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -4,10 +4,20 @@ import SkinSearch from "./skins/search"
 
 export default function Dashboard({username} : {username: string}) {
     return (
-        <div>
-            <p>Welcome {username}!</p>
-            <SkinsPage />
-            <SkinSearch />
+        <div className="flex flex-col w-screen h-screen p-10 gap-10">
+            <p className="text-5xl">Welcome, <span className="text-blue-500 font-semibold">{username}!</span></p>
+            <div className="flex flex-row justify-around grow w-full">
+                <div className="w-1/4 p-3 border-2 rounded-2xl">
+                    <SkinsPage />
+                </div>
+                <div className="w-1/4 p-3 border-2 rounded-2xl">
+                    <p>My Skins</p>
+                </div>
+                <div className="w-1/4 p-3 border-2 rounded-2xl">
+                    <SkinSearch />
+                </div>
+
+            </div>
         </div>
     )
 }
